866c404562919b28e3fb9a567bb3141b40a67ea1
Shrey Purohit <91727034+ShreyPurohit@users.noreply.github.com>
2024-09-04 16:22:37 +0530

    Delete npm-dynamic-form/dist directory

--- npm-dynamic-form/dist/components/DynamicForm.d.ts
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { DynamicFormProps } from '../assets/interfaces';
-declare const CustomizedDynamicForm: React.FC<DynamicFormProps>;
-export default CustomizedDynamicForm;
--- npm-dynamic-form/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import DynamicForm from './components/DynamicForm';
-import { IFormField, DynamicFormProps } from './assets/interfaces';
-export default DynamicForm;
-export { IFormField, DynamicFormProps };
